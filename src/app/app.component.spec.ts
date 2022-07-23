730372e6071035b82cce4c3bfa7680894a41d56f
bela-mad <isabelaargolo3110@gmail.com>
2022-07-23 14:51:53 -0300

    criado os componentes navbar e footer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projetoclio'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('projetoclio');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FooterComponent } from './core/footer/footer.component';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      NavBarComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
