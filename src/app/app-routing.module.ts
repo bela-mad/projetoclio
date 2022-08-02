b2319945d7fad9b9f9977a0e83885e0430ad5493
bela-mad <isabelaargolo3110@gmail.com>
2022-08-02 11:58:56 -0300

    criada pagina cadastro museu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './area-deslogada/login/login.component';
+import { CadastroMuseusComponent } from './area-logada/admin/cadastro-museus/cadastro-museus.component';
 import { PoliticaPrivacidadeComponent } from './core/politica-privacidade/politica-privacidade.component';
 import { TermosCondicoesComponent } from './core/termos-condicoes/termos-condicoes.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'termos-condicoes', component: TermosCondicoesComponent },
-  { path: 'politica-privacidade', component: PoliticaPrivacidadeComponent }
+  { path: 'politica-privacidade', component: PoliticaPrivacidadeComponent },
+  { path: 'cadastro-museus', component: CadastroMuseusComponent }
 
 ];
 
