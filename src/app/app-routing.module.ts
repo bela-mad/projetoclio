d6fa36e90440460f9bd7ca50708df6e980d520df
brendabba <brendabisp@hotmail.com>
2022-08-06 20:14:22 -0300

    Revisão para evitar erros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AlterarMuseusComponent } from './area-logada/admin/alterar-museus/alterar-museus.component';
 import { PerfilVisitanteComponent } from './area-logada/perfis/perfil-visitante/perfil-visitante.component';
 import { PerfilAdminComponent } from './area-logada/perfis/perfil-admin/perfil-admin.component';
 import { AlterarSenhaComponent } from './area-deslogada/alterar-senha/alterar-senha.component';
@@ -11,12 +12,7 @@ import { TermosCondicoesComponent } from './core/termos-condicoes/termos-condico
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'termos-condicoes', component: TermosCondicoesComponent },
-  { path: 'politica-privacidade', component: PoliticaPrivacidadeComponent },
-  { path: 'cadastro', component: CadastroComponent },
-  { path: 'alterar-senha', component: AlterarSenhaComponent },
-  { path: 'perfil-admin', component: PerfilAdminComponent },
-  { path: 'perfil-visitante', component: PerfilVisitanteComponent }
-
+  { path: 'politica-privacidade', component: PoliticaPrivacidadeComponent }
 ];
 
 @NgModule({
