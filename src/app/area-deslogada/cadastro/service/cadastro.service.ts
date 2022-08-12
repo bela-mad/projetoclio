5bc74702373ff6c337f46f19103ce85520525440
brendabba <brendabisp@hotmail.com>
2022-08-11 23:11:35 -0300

    concluido cadastro.service.ts

---
+++ src/app/area-deslogada/cadastro/service/cadastro.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { Cadastro } from '../cadastro';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CadastroService {
+ 
+
+constructor(private http: HttpClient) { 
+
+}
+
+inserirCadastro(cadastro:Cadastro) : Observable<Cadastro>{
+  return this.http.post<Cadastro>('http://api-passaporteclio.herokuapp.com/visitante', cadastro);
+
+}
+
+}
