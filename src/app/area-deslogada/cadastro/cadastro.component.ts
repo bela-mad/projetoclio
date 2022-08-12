6e756248ccf0f8198c546a91b15e604c4484aaa3
brendabba <brendabisp@hotmail.com>
2022-08-11 23:10:32 -0300

    concluido cadastro.component.ts

--- src/app/area-deslogada/cadastro/cadastro.component.ts
+++ src/app/area-deslogada/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cadastro } from './cadastro';
+import { CadastroService } from './service/cadastro.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
- 
-  constructor() { }
+ cadastros = [] as any;
 
-  ngOnInit() {
+ cadastro:Cadastro = {
+  nome: "",
+  sobrenome: "",
+  email:"",
+  senha:"",
+ }
+  constructor(private service: CadastroService) {
+    this.cadastro = new Cadastro();
+   }
+
+  ngOnInit():void {
+  }
+
+  onSubmit(){
+    this.service
+    .inserirCadastro(this.cadastro)
+    .subscribe( responde => console.log(responde));
   }
 
 }
