f6191cc825db0d04739cfa7abd05aaa3c0fddc81
brendabba <brendabisp@hotmail.com>
2022-08-11 23:12:10 -0300

    concluido detalhes-museu

--- src/app/area-logada/museus/museu/detalhes-museu/detalhes-museu.component.ts
+++ src/app/area-logada/museus/museu/detalhes-museu/detalhes-museu.component.ts
@@ -1,32 +1,36 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhes-museu',
   templateUrl: './detalhes-museu.component.html',
-  styleUrls: ['./detalhes-museu.component.css']
+  styleUrls: ['./detalhes-museu.component.css'],
 })
 export class DetalhesMuseuComponent implements OnInit {
+  nome: String = '';
+  descricaoMuseu: String = '';
+  funcionamentoMuseu: String = '';
+  urlFoto: String = '';
+  urlSite: String = '';
+  urlInstagram: String | undefined;
+  endereco: String = '';
+  cep: String = '';
+  rua: String = '';
+  numero: String = '';
+  bairro: String = '';
+  cidade: String = '';
+  estado: String = '';
+  pais: String = '';
+  avaliacao: String = '';
+  nota: String = '';
+ 
 
- @Input() nome: String = '' ;
- @Input() descricaoMuseu: String = '' ;
- @Input() funcionamentoMuseu: String = '' ;
- @Input() urlFoto: String = '' ;
- @Input() urlSite: String = '' ;
- @Input() urlInstagram: String | undefined ;
- @Input() endereco: String = ''  ;
- @Input() cep: String = '' ;
- @Input() rua: String = ''  ;
- @Input() numero: String = '' ;
- @Input() bairro: String = '' ;
- @Input() cidade: String = '' ;
- @Input() estado: String = '' ;
- @Input() pais: String = '' ;
- @Input() avaliacao: String = ''  ;
- @Input() nota: String = '' ;
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    /*if (this.router.getCurrentNavigation().extras?.state != null) {
+      this.museu = this.router.getCurrentNavigation().extras.state;
+    }*/
   }
+  
 
+  ngOnInit() {}
 }
