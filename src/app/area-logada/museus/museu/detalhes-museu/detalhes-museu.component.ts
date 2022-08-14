d90956a0526141078d2cd8830bc5162df216b7e4
bela-mad <isabelaargolo3110@gmail.com>
2022-08-14 11:46:10 -0300

    iniciado o consumo da api das paginas de museus

--- src/app/area-logada/museus/museu/detalhes-museu/detalhes-museu.component.ts
+++ src/app/area-logada/museus/museu/detalhes-museu/detalhes-museu.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { MuseusApiService } from '../../services/museus-api.service';
 
 @Component({
   selector: 'app-detalhes-museu',
@@ -7,30 +9,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./detalhes-museu.component.css'],
 })
 export class DetalhesMuseuComponent implements OnInit {
-  nome: String = '';
-  descricaoMuseu: String = '';
-  funcionamentoMuseu: String = '';
-  urlFoto: String = '';
-  urlSite: String = '';
-  urlInstagram: String | undefined;
-  endereco: String = '';
-  cep: String = '';
-  rua: String = '';
-  numero: String = '';
-  bairro: String = '';
-  cidade: String = '';
-  estado: String = '';
-  pais: String = '';
-  avaliacao: String = '';
-  nota: String = '';
- 
+  museu: any = {};
 
   constructor(private router: Router) {
-    /*if (this.router.getCurrentNavigation().extras?.state != null) {
-      this.museu = this.router.getCurrentNavigation().extras.state;
-    }*/
+    // if (this.router.getCurrentNavigation().extras?.state != null) {
+    //   this.museu = this.router.getCurrentNavigation().extras.state;
+    // }
   }
-  
 
-  ngOnInit() {}
+  ngOnInit(): void {}
 }
